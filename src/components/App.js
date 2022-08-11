4063406b96cbd207d0a07f54ac275cb6df64dbd7
IrenEvanc <mira.evanc@gmail.com>
2022-08-11 15:02:00 +0300

    Расположение футера

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 // import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Header from "./header components/Header";
 import MainContent from "./MainContent";
@@ -9,17 +9,17 @@ import Products from "./AllProducts/Products";
 
 function App() {
     return (
-        <div className="container">
+        <div >
+            <Router>
             <Header/>
-
-            <BrowserRouter>
+                <div>
                 <Routes>
                     <Route path="/" element={<Products/>} />
                     <Route path="product" element={<MainContent/>} />
                 </Routes>
-            </BrowserRouter>
-
+                </div>
             <Footer/>
+            </Router>
         </div>
     )
 }

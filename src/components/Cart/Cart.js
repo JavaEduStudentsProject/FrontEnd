5d5fa13e7c3373072895d7e2345e89685de5d902
IrinaPostnikova <iri.postnikova@gmail.com>
2022-10-01 23:01:39 +0300

    ярлык корзины

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,10 @@
+
+import  "./styleCart.css"
+const Cart = () => {
+    return (
+        <div className="cart">
+            <h1 className="title-cart">Корзина товаров</h1>
+        </div>
+    );
+}
+export default Cart;
--- src/components/header components/Header.js
+++ src/components/header components/Header.js
@@ -8,7 +8,8 @@ import avatar1 from '../../images/avatar.jpg'
 import {FilterArrayContext, ImmutableProductListContext, PriceFilterArrayContext} from "../../services/Context";
 import {useParams} from "react-router-dom";
 import ProductList from "../../services/ProductList";
-import { FaShoppingCart } from "react-icons/fa";
+import {FaShoppingCart} from "react-icons/fa";
+import Cart from "../Cart/Cart"
 
 
 export default function Header(props) {
@@ -39,21 +40,24 @@ export default function Header(props) {
     }
 
 
-
     return (
         <header>
             <nav className="nav-panel">
-                <img className="nav-img" src={img} onClick={()=>{window.location.assign("/")}}/>
+                <img className="nav-img" src={img} onClick={() => {
+                    window.location.assign("/")
+                }}/>
                 <DropDownMenu/>
                 <SearchField handleChange={props.handleChange} searchField={props.searchField}/>
                 <div className="userIcons">
                     <Stack>
                         {/*<Avatar alt="Корзина"*/}
                         {/*        src='https://thumbs.dreamstime.com/b/%D0%B7%D0%BD%D0%B0%D1%87%D0%BE%D0%BA-%D0%BA%D0%BE%D1%80%D0%B7%D0%B8%D0%BD%D1%8B-%D0%BF%D1%80%D0%BE%D1%81%D1%82%D0%BE%D0%B9-%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80-%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%B0-%D0%B7%D0%B0%D0%BF%D0%BE%D0%BB%D0%BD%D0%B8%D0%BB%D0%B0-%D0%BB%D0%B8%D0%BD%D0%B8%D1%8E-%D0%BF%D0%B8%D0%BA%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D1%83-171571446.jpg'/>*/}
-                        <FaShoppingCart onClick={()=> setCartOpen(cartOpen=!cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`}/>
+                        <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)}
+                                        className={`shop-cart-button ${cartOpen && 'active'}`}/>
 
                         {cartOpen && (
                             <div className={'shop-cart'}>
+                                <Cart></Cart>
                             </div>
                         )}
 

29f2e7437b8477d39b1d88e86b608fbf3b6fb853
IrenEvanc <mira.evanc@gmail.com>
2022-10-08 18:46:53 +0300

    newProducts

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -2,8 +2,9 @@ import axios from "axios";
 
 import ProductList from "../../src/services/ProductList";
 
-const PRODUCT_BASE_REST_API_URL = '/temp_props_1.json';
-// const PRODUCT_BASE_REST_API_URL = '/props.json';
+// const PRODUCT_BASE_REST_API_URL = '/temp_props_1.json';
+const PRODUCT_BASE_REST_API_URL = '/props.json';
+
 // const PRODUCT_BASE_REST_API_URL = '/initialDetails.json';
 
 class ProductService{

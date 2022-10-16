a5ae9d120accae42c5049c32dadc43efe441cd2a
IrenEvanc <mira.evanc@gmail.com>
2022-10-16 22:11:31 +0300

    Cart

--- src/components/header components/Header.js
+++ src/components/header components/Header.js
@@ -118,8 +118,16 @@ export default function Header(props) {
                     {currentUser ? (
                         <div className="userIcons">
                             <Stack>
-                                <Avatar alt="Корзина"
-                                        src='https://thumbs.dreamstime.com/b/%D0%B7%D0%BD%D0%B0%D1%87%D0%BE%D0%BA-%D0%BA%D0%BE%D1%80%D0%B7%D0%B8%D0%BD%D1%8B-%D0%BF%D1%80%D0%BE%D1%81%D1%82%D0%BE%D0%B9-%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80-%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%B0-%D0%B7%D0%B0%D0%BF%D0%BE%D0%BB%D0%BD%D0%B8%D0%BB%D0%B0-%D0%BB%D0%B8%D0%BD%D0%B8%D1%8E-%D0%BF%D0%B8%D0%BA%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D1%83-171571446.jpg'/>
+                                <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)}
+                                                        className={`shop-cart-button ${cartOpen && 'active'}`}/>
+
+                                        {cartOpen && (
+                                            <div className={'shop-cart'}>
+                                                <Cart order={props.order} setOrder={props.setOrder}
+                                                      deleteOrder={props.deleteOrder} quantity={quantity}
+                                                      setQuantity={setQuantity}></Cart>
+                                            </div>
+                                        )}
                                 <span className="count-products-in-basket">{countProductInBasket}</span>
                             </Stack>
                         <div className="navbar-nav ml-auto">
@@ -142,29 +150,6 @@ export default function Header(props) {
                                 </Stack>
                         </div>
                     )}
-                {/*<div className="userIcons">*/}
-                {/*    <Stack>*/}
-                {/*        /!*<Avatar alt="Корзина"*!/*/}
-                {/*        /!*        src='https://thumbs.dreamstime.com/b/%D0%B7%D0%BD%D0%B0%D1%87%D0%BE%D0%BA-%D0%BA%D0%BE%D1%80%D0%B7%D0%B8%D0%BD%D1%8B-%D0%BF%D1%80%D0%BE%D1%81%D1%82%D0%BE%D0%B9-%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80-%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%B0-%D0%B7%D0%B0%D0%BF%D0%BE%D0%BB%D0%BD%D0%B8%D0%BB%D0%B0-%D0%BB%D0%B8%D0%BD%D0%B8%D1%8E-%D0%BF%D0%B8%D0%BA%D1%82%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D1%83-171571446.jpg'/>*!/*/}
-                {/*        <FaShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)}*/}
-                {/*                        className={`shop-cart-button ${cartOpen && 'active'}`}/>*/}
-
-                {/*        {cartOpen && (*/}
-                {/*            <div className={'shop-cart'}>*/}
-                {/*                <Cart order={props.order} setOrder={props.setOrder}*/}
-                {/*                      deleteOrder={props.deleteOrder} quantity={quantity}*/}
-                {/*                      setQuantity={setQuantity}></Cart>*/}
-                {/*            </div>*/}
-                {/*        )}*/}
-
-                {/*        <span className="count-products-in-basket">{countProductInBasket}</span>*/}
-                {/*    </Stack>*/}
-
-                {/*    <Stack className="avatarIcon">*/}
-                {/*        <Avatar alt="Пользователь" src={avatar1}/>*/}
-                {/*    </Stack>*/}
-
-                {/*</div>*/}
 
             </nav>
             <nav className="nav-list">

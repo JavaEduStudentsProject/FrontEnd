7e875d6e512a8ef344a7eed3f47eeb0ead201421
IrenEvanc <mira.evanc@gmail.com>
2022-10-16 22:01:17 +0300

    InputFile

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,11 +1,13 @@
 import axios from "axios";
+import cors from 'cors'
 
 import ProductList from "../../src/services/ProductList";
 
 const PRODUCT_BASE_REST_API_URL = '/temp_props_1.json';
 // const PRODUCT_BASE_REST_API_URL = '/props.json';
 // const PRODUCT_BASE_REST_API_URL = '/initialDetails.json';
-const FILE_REST_API_URL = 'http://localhost:8083/api'
+const FILE_REST_API_URL = 'http://localhost:8083/file'
+
 
 class ProductService{
 

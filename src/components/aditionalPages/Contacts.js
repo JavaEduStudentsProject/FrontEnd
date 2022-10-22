106dd62f5a0d79e8c7601d5ce7a59a93f5dcce8d
Daniil Privalov <prival-chuk@mail.ru>
2022-10-22 20:06:46 +0300

    Map

--- src/components/aditionalPages/Contacts.js
+++ src/components/aditionalPages/Contacts.js
@@ -1,9 +1,21 @@
 import React from "react";
+import img from "../../images/img.png";
+import GoogleMapReact from 'google-map-react';
 
-const Contacts = (props)=>{
-    return(
-        <html>
-        <header>
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+function Contacts(){
+
+    const defaultProps = {
+                center: {
+                        lat: 10.99835602,
+                        lng: 77.01502627
+                },
+                zoom: 11
+        };
+
+        return (
+            <html>
             <h2>Контакты</h2>
             <h4>Адрес офиса:</h4><p>129110 г. Москва, проспект Мира, д. 62, стр. 1</p>
             <h4>Режим работы:</h4><p>Пн. - Пт. с 09.00 до 20.00</p>
@@ -12,11 +24,22 @@ const Contacts = (props)=>{
             <p>E-mail: magazin-info@gmail.com</p>
             <h4>Сотрудничество:</h4>
             <p>E-mail: magazin-hr@gmail.com</p>
-        </header>
-        <body>
-        {/*<img src={require('/Users/elizavetakabak/repos/MarketPlace/FrontEnd/src/images/img.png').default} />*/}
-        </body>
-        </html>
-    )
+            <img src ={img} alt="Схема проезда"/>
+
+            <div style={{ height: '30vh', width: '30%' }}>
+                    <GoogleMapReact
+                        bootstrapURLKeys={{ key: "" }}
+                        defaultCenter={defaultProps.center}
+                        defaultZoom={defaultProps.zoom}
+                    >
+                            <AnyReactComponent
+                                lat={59.955413}
+                                lng={30.337844}
+                                text="My Marker"
+                            />
+                    </GoogleMapReact>
+            </div>
+            </html>
+        );
 }
 export default Contacts

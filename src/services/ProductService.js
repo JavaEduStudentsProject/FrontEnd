577d80bba0fb38b44f4c63b39744ef23932fc1c3
Dmitry286 <DShishlov@gmail.com>
2022-10-24 13:17:08 +0300

    websockets

--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 import ProductList from "../../src/services/ProductList";
 
-// const PRODUCT_BASE_REST_API_URL = '/temp_props_1.json';
-// const PRODUCT_BASE_REST_API_URL = '/props.json';
 
 const PRODUCT_BASE_REST_API_URL = 'http://localhost:8083/api/products';
 

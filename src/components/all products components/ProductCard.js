2e189d00ebb9793ed8573b51b1752fb3306472ac
Dmitry286 <DShishlov@gmail.com>
2022-10-27 15:19:59 +0300

    refactoring

--- src/components/all products components/ProductCard.js
+++ src/components/all products components/ProductCard.js
@@ -8,14 +8,17 @@ import {Link} from 'react-router-dom'
 
 const ProductCard = (props) => {
     const flatProduct = ProductList.flatProduct(props.item);
+    console.log(flatProduct)
 
     //const keys = Object.keys(flatProduct).filter(key => key !== "image" && key !== "description" && key !== "title" && key !== "0"&& key !== "1"&& key !== "2"&& key !== "3"&& key !== "4"&& key !== "id");
+
+    const keys = Object.keys(flatProduct).filter(key => key === "brand" || key === "description" || key === "category");
 
     const descriptionList = keys.map((key, index) =>
         keys[index] === "description" ?
             flatProduct[key].length < 90 ?
-            <li key={flatProduct.id + index}>{flatProduct[key]}</li> :
-            <li key={flatProduct.id + index}>{flatProduct[key].substring(0, 90)}...</li> :
+                <li key={flatProduct.id + index}>{flatProduct[key]}</li> :
+                <li key={flatProduct.id + index}>{flatProduct[key].substring(0, 90)}...</li> :
             <li key={flatProduct.id + index}>{flatProduct[key]}</li>
     );
 
